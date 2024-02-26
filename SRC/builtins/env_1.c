e541f80e1b0fddfe82af71b37e37a27f588e5531
Guillem Forns <132559287+GuillemFo@users.noreply.github.com>
2024-02-26 16:52:11 +0000

    missing ++i at line 124

--- SRC/builtins/env_1.c
+++ SRC/builtins/env_1.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env_1.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 08:34:19 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/02/19 10:01:09 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/02/26 16:49:35 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -121,7 +121,7 @@ char	*equal_til_end(char	*var)
 		x = -1;
 		while (var[++start] != '\0')
 			content[++x] = var[start];
-		content[x] = '\0';
+		content[++x] = '\0';
 		return (content);
 	}
 	else
