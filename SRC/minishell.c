aff916c4d20e723d071a4f98c67d7ad78d5e40e7
Guillem Forns <132559287+GuillemFo@users.noreply.github.com>
2024-02-26 19:40:08 +0000

    quotes

--- SRC/minishell.c
+++ SRC/minishell.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 13:29:06 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/02/26 17:02:05 by codespace        ###   ########.fr       */
+/*   Updated: 2024/02/26 17:04:04 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,7 @@ int	main(int ac, char **av, char **envp)
 		add_history(str);
 		if (str)
 		{
+			str = clear_quotes(str);
 			str = find_dollar(str, env);
 			input = ft_lexer(str);
 			data = ft_parser(input);
@@ -71,6 +72,7 @@ int	main(int ac, char **av, char **envp)
 		str = readline(C_G "minishell: " C_RESET);
 		if (str != NULL)
 		{
+			str = clear_quotes(str);
 			str = find_dollar(str, env);
 			input = ft_lexer(str);
 			data = ft_parser(input);
