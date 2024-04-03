c4ff0fd2019b1d73827204fddd8fb5e44753a977
Guillem Forns Sant <gforns-s@car9s4.42barcelona.com>
2024-04-03 10:56:25 +0200

    comment

--- SRC/quotes/quotes.c
+++ SRC/quotes/quotes.c
@@ -6,7 +6,7 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 11:09:33 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/03/26 11:37:58 by gforns-s         ###   ########.fr       */
+/*   Updated: 2024/04/03 10:55:42 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,7 +102,7 @@ char *cont_bef_q(char *str, char c)
 }
 
 
-
+// Leaks here!!
 char	*clear_quotes(char *str, t_env *env, int exit_code)
 {
 	char 	*tmp_bef;
@@ -141,104 +141,3 @@ char	*clear_quotes(char *str, t_env *env, int exit_code)
 		res = find_dollar(res, env, exit_code);
 	return (res);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// char	has_quotes(char *str)
-// {
-// 	int	i;
-// 	i = 0;
-// 	while (str[i] != '\0')
-// 	{
-// 		if (str[i] == '\"' || str[i] == '\'')
-// 			return (str[i]);
-// 		i++;
-// 	}
-// 	return (str[i]);
-// }
-
-// char	*clear_quotes(char *str)
-// {
-// 	int		i;
-// 	int		j;
-// 	char	c;
-// 	char 	*cpy;
-// 	char	*tmp;
-// 	char	*tmp2;
-// 	char	*tmp3;
-// 	char	*tmp4;
-
-// 	i = 0;
-// 	cpy = ft_strdup(str);
-// 	c = has_quotes(cpy);
-	
-// 	if (c != '\0')
-// 	{
-// 		while (cpy[i] != c)
-// 			i++;
-// 		tmp = malloc(i + 1 *sizeof(char));
-// 		i = 0;
-// 		while (cpy[i] != c)
-// 		{
-// 			tmp[i] = cpy[i];
-// 			i++;
-// 		}
-// 		tmp[i] = '\0';
-// 		i++;
-// 		j = i;
-// 		while (cpy[i] != c && cpy[i] != '\0')
-// 			i++;
-// 		tmp2 = malloc((i - j + 1) *sizeof(char));
-// 		i = j;
-// 		j = 0;
-// 		while (cpy[i] != c && cpy[i] != '\0')
-// 		{
-// 			tmp2[j] = cpy[i];
-// 			i++;
-// 			j++;
-// 		}
-// 		tmp2[j] = '\0';
-// 		if (cpy[i] == '\0')
-// 		{
-// 			free(cpy);
-// 			cpy = ft_strjoinplus(tmp, tmp2);
-// 			return (cpy);
-// 		}
-// 		i++;
-// 		j = i;
-// 		while (cpy[i] != '\0')
-// 			i++;
-// 		tmp3 = malloc((i - j + 1) * sizeof(char));
-// 		i = j;
-// 		j = 0;
-// 		while (cpy[i] != '\0')
-// 		{
-// 			tmp3[j] = cpy[i];
-// 			i++;
-// 			j++;
-// 		}
-// 		tmp3[j] = '\0';
-// 		tmp4 = ft_strjoinplus(tmp, tmp2);
-// 		cpy = ft_strjoinplus(tmp4, tmp3);
-// 	}
-// 	else
-// 		return (cpy);
-// 	return (str);
-// }
