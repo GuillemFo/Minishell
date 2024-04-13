ebaa02b8aebf654f544d44faa6c9975ef79bebc0
Anna Danylevych <annadanylevych1@gmail.com>
2024-04-13 17:23:24 +0200

    almost

--- Include/minishell.h
+++ Include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: adanylev <adanylev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 16:11:52 by codespace         #+#    #+#             */
-/*   Updated: 2024/04/13 15:17:21 by adanylev         ###   ########.fr       */
+/*   Updated: 2024/04/13 17:21:11 by adanylev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,7 @@ typedef	struct	s_errors
 {
 	int	error;
 	int	exit_code;
+	char *str;
 }				t_errors;		
 
 typedef enum e_sign
@@ -239,5 +240,6 @@ int					is_lonely_builtin(t_parser *parser, t_pipe *pipex,
 void				making_kids(t_parser *parser, t_pipe *pipex,
 						t_env **envi, int *error);
 void				get_token(t_parser *parser, t_lexer *lexer, t_redir *tmp);
+
 
 #endif
